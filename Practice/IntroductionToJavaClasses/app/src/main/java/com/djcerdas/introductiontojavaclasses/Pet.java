891414f9e7cb9919d4f9c24d5831895b23ec532c
djcerdas <david.cerdas@djcerdas.com>
2020-04-21 23:09:47 -0600

    add the getters and setters for the Class Pet

--- Practice/IntroductionToJavaClasses/app/src/main/java/com/djcerdas/introductiontojavaclasses/Pet.java
+++ Practice/IntroductionToJavaClasses/app/src/main/java/com/djcerdas/introductiontojavaclasses/Pet.java
@@ -18,4 +18,37 @@ public class Pet {
         this.weight = weightConstructor;
         this.isDog =  isDogConstructor;
     }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public void setDog(boolean dog) {
+        isDog = dog;
+    }
+
+    public boolean isDog() {
+        return isDog;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+
+
+
 }
