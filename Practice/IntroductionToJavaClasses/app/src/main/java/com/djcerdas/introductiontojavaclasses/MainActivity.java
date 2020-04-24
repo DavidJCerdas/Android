81c92e8ab0bb696a78b249d2a670acf107738eaf
djcerdas <david.cerdas@djcerdas.com>
2020-04-23 21:14:16 -0600

    Add a simple button and test on click.

--- Practice/IntroductionToJavaClasses/app/src/main/java/com/djcerdas/introductiontojavaclasses/MainActivity.java
+++ Practice/IntroductionToJavaClasses/app/src/main/java/com/djcerdas/introductiontojavaclasses/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -19,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "Testing log message to confirm the pet: "+bold.getName()+" was created with ID:"+bold.getId());
         Pet toast = new Pet("Toast",5,5.2f,true);
         Log.d(TAG, "Testing log message to confirm the pet: "+toast.getName()+" was created with ID:"+toast.getId());
+
+        View tesButton = findViewById(R.id.buttonTest);
+        // Create an anonymous Class to simplify the handling
+        tesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.w(TAG,"The button was clicked" );
+            }
+        });
     }
 }
 
