3dff9d0157e487deb2988a197d53686500e72a83
djcerdas <david.cerdas@djcerdas.com>
2020-05-18 22:43:19 -0600

    incorporate jetpack navigation java class for Login Fragment

--- FinalProject/PokedexTico/app/src/main/java/com/djcerdas/pokedextico/fragment/LoginFragment.java
+++ FinalProject/PokedexTico/app/src/main/java/com/djcerdas/pokedextico/fragment/LoginFragment.java
@@ -1,6 +1,39 @@
 package com.djcerdas.pokedextico.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.djcerdas.pokedextico.R;
 
 public class LoginFragment extends Fragment {
+
+    private Button loginButton;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login_user, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        loginButton = view.findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // I need to set the right new fragment
+                NavHostFragment.findNavController(LoginFragment.this).navigate(R.id.loginFragment);
+            }
+        });
+    }
 }
+
