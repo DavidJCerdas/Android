e8b4105be2c8579932d1a101431ca68b4b2e98f2
djcerdas <david.cerdas@djcerdas.com>
2020-06-07 00:44:03 -0600

    Revert "Incorporate viewpager2 dependencies and initial configuration"
    This reverts commit 7f6cd2ce

--- FinalProject/PokedexTico/app/src/main/java/com/djcerdas/pokedextico/fragment/MenuPokedex.java
+++ FinalProject/PokedexTico/app/src/main/java/com/djcerdas/pokedextico/fragment/MenuPokedex.java
@@ -1,7 +1,6 @@
 package com.djcerdas.pokedextico.fragment;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,32 +10,20 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavDirections;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.djcerdas.pokedextico.R;
-import com.djcerdas.pokedextico.model.PokemonList;
-import com.djcerdas.pokedextico.viewmodel.PokemonViewModel;
 
 public class MenuPokedex extends Fragment {
     private TextView print_var;
     private Button recycleButton;
     private Button nullDisplay;
-    private PokemonViewModel viewModel;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.menu_pokedex, container, false);
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        viewModel = new ViewModelProvider(this).get(PokemonViewModel.class);
-        viewModel.getPokemonList();
     }
 
     @Override
@@ -65,13 +52,6 @@ public class MenuPokedex extends Fragment {
         // Make the calculation of the weight on the Moon
         String trainerName = MenuPokedexArgs.fromBundle(getArguments()).getTrainerName();
         print_var.setText(trainerName);
-
-        viewModel.getPokemonListLiveData().observe(getViewLifecycleOwner(), new Observer<PokemonList>() {
-            @Override
-            public void onChanged(PokemonList pokemonList) {
-                Log.d("POKERROR", pokemonList.getPokemonInfoList().get(0).getName());
-            }
-        });
     }
 
 
--- FinalProject/PokedexTico/app/src/main/java/com/djcerdas/pokedextico/fragment/ShowPokemonFragment.java
+++ FinalProject/PokedexTico/app/src/main/java/com/djcerdas/pokedextico/fragment/ShowPokemonFragment.java
@@ -1,15 +1,21 @@
 package com.djcerdas.pokedextico.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
 import com.djcerdas.pokedextico.R;
+import com.djcerdas.pokedextico.model.PokemonInfo;
 import com.squareup.picasso.Picasso;
 
 public class ShowPokemonFragment extends Fragment {
@@ -31,12 +37,16 @@ public class ShowPokemonFragment extends Fragment {
         pokemonName = view.findViewById(R.id.pokemon_name);
         pokemonID = ShowPokemonFragmentArgs.fromBundle(getArguments()).getPokemonID();
         String pID = pokemonID.toString();
-
         Picasso.get().load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + pID + ".png").into(showPokemon);
 
         String pokemonNameString = ShowPokemonFragmentArgs.fromBundle(getArguments()).getPokemonName();
         pokemonName.setText(pokemonNameString);
+
+
     }
 
+
+    private void runOnUiThreat(Runnable runnable) {
+    }
 
 }
